a46e5572cc954a52f2c48bd9f09c4389be7a107f
Roman <soleynikr@gmail.com>
2022-10-31 16:46:09 +0000

    go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+    
+    var name string = "Roman"
+    var city, state string = "Phoenix", "Arizona"
+    fmt.Println("name : ", name)
+    fmt.Println("city : ", city, " state : ", state)
+}
---
+++ two-functions.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func shippingCalc(x int, y int, z int) int {
+    return x + y + z
+}
+
+func main(){
+    fmt.Println("Enter Height: 42")
+    fmt.Println("Enter Width: 13")
+    fmt.Println("Enter Length: 20")
+    fmt.Println("Total: ", shippingCalc(42, 13, 20))
+}
+
