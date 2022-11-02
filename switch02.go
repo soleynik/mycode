7fa535ceab65aa51923e188dfc29f01936855e3a
Roman <soleynikr@gmail.com>
2022-11-02 08:58:11 +0000

    go switch

---
+++ switch02.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main(){
+
+    watch := time.Now()
+
+    switch {
+    case watch.Hour() < 6:
+        fmt.Println("Go back to sleep.")
+    case watch.Hour() < 12:
+        fmt.Println("Good morning!")
+    case watch.Hour() < 18:
+        fmt.Println("Good afternoon.")
+    default:
+        fmt.Println("Counting sheep. Z-z-z-z-z-z-z")
+    }
+}
---
+++ switch03_multiexpressions.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "time"
+    "fmt"
+)
+
+func main() {
+
+    switch time.Now().Weekday() {
+        
+    case time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday:
+        fmt.Println("Working for the weekend.")
+    case time.Saturday, time.Sunday:
+        fmt.Println("Party time!")
+    }
+}
