c9fdd22e5fc22953f681a92cb837692368b731cd
Roman <soleynikr@gmail.com>
2022-11-04 04:56:20 +0000

    learning about http servers

---
+++ httpsvr02.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/", HelloServer)
+    fmt.Println("Server started at port 8079")
+    log.Fatal(http.ListenAndServe(":8079", nil))
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    // get the URL path value with r.URL.Path[1:] and build a message with the data
+    // by describing the data we want as r.URL.Path[1:] we are "dropping" the leading "/"
+    fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+}    
